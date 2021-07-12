cae5802a07fbe8632bfe625bf6fc3f1109a34718
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-07-12 18:28:50 +0000

    forms

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,26 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  #def song_title
+
+  def genre_id
+    self.genre ? self.genre.id : nil
+  end
+
+  
+
+
+
+
 end
